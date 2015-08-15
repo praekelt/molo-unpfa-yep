b521ea87c874e386e3daa7e8d02a2224ed57bc6d
Saeed Marzban <Saeed.mb8@gmail.com>
2015-08-15 17:30:53 +0200

    added tests.py

---
+++ tuneme/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from tuneme.context_processors import default_forms
+
+
+# class ContextProcessorsTest(TestCase):
+#
+#     def test_default_forms(self):
+#         
