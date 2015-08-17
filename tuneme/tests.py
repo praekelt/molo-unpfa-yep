3864e5ff22102e9cbacd0b5d0c1e5584714f8166
Saeed Marzban <Saeed.mb8@gmail.com>
2015-08-17 11:32:27 +0200

    added login_form test

--- tuneme/tests.py
+++ tuneme/tests.py
@@ -1,8 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from tuneme.context_processors import default_forms
 
 
-# class ContextProcessorsTest(TestCase):
-#
-#     def test_default_forms(self):
-#         
+class ContextProcessorsTest(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_default_forms(self):
+        request = self.factory.get('/profiles/login/')
+        result = default_forms(request)
+        assert(result['login_form'])
