e4f0182f363c1ec2efa216f515b2c58be41976a4
Saeed Marzban <Saeed.mb8@gmail.com>
2015-08-17 13:59:45 +0200

    added more test for ContextProcessors

--- tuneme/tests.py
+++ tuneme/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, RequestFactory
 from tuneme.context_processors import default_forms
+from tuneme.forms import LoginForm, RegistrationForm, EditProfileForm
+from tuneme.forms import ProfilePasswordChangeForm
 
 
 class ContextProcessorsTest(TestCase):
@@ -10,4 +12,17 @@ class ContextProcessorsTest(TestCase):
     def test_default_forms(self):
         request = self.factory.get('/profiles/login/')
         result = default_forms(request)
-        assert(result['login_form'])
+        self.assertTrue(
+            isinstance(result['login_form'], LoginForm))
+        request = self.factory.get('/profiles/register/')
+        result = default_forms(request)
+        self.assertTrue(
+            isinstance(result['registration_form'], RegistrationForm))
+        request = self.factory.get('/profiles/edit/myprofile/')
+        result = default_forms(request)
+        self.assertTrue(
+            isinstance(result['edit_profile_form'], EditProfileForm))
+        request = self.factory.get('/profiles/password-reset/')
+        result = default_forms(request)
+        self.assertTrue(isinstance(result['password_change_form'],
+                        ProfilePasswordChangeForm))
