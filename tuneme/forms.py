2ed0610f71ad1c1fb53f8203425c4a2615d890fa
Saeed Marzban <Saeed.mb8@gmail.com>
2015-08-18 10:40:37 +0200

    imported forms from molo.profiles

--- tuneme/forms.py
+++ tuneme/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from molo.profiles import forms as profile_forms
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -11,7 +12,7 @@ class LoginForm(forms.Form):
     }))
 
 
-class RegistrationForm(forms.Form):
+class RegistrationForm(profile_forms.RegistrationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': _('Username'),
     }))
@@ -20,13 +21,13 @@ class RegistrationForm(forms.Form):
     }))
 
 
-class EditProfileForm(forms.Form):
+class EditProfileForm(profile_forms.EditProfileForm):
     alias = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': _('Display Name'),
     }))
 
 
-class ProfilePasswordChangeForm(forms.Form):
+class ProfilePasswordChangeForm(profile_forms.ProfilePasswordChangeForm):
     old_password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': _('Old Password'),
     }))
