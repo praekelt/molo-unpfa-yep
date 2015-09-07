e8500fa7af88e6263034c82185e4cbddac58d823
Saeed Marzban <Saeed.mb8@gmail.com>
2015-09-07 12:23:05 +0200

    removed surveys

--- surveys/urls.py
+++
@@ -1,7 +0,0 @@
-from surveys import views
-from django.conf.urls import url
-
-urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/survey/$', views.survey, name='survey'),
-]
--- tuneme/settings/base.py
+++ tuneme/settings/base.py
@@ -67,7 +67,6 @@ INSTALLED_APPS = (
     'raven.contrib.django.raven_compat',
     'tuneme',
     'polls',
-    'surveys',
     'google_analytics',
     'djcelery',
 
