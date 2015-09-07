c0f4b86070829737033d3ec1a1a08af35ab85228
Milton Madanda <madandat@gmail.com>
2015-09-07 12:06:36 +0200

    allow polls to be added

--- polls/models.py
+++ polls/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
-from molo.core.models import HomePage
+from molo.core.models import HomePage, LanguagePage, ArticlePage
 
 HomePage.subpage_types += ['polls.Question']
+LanguagePage.subpage_types += ['polls.Question']
+ArticlePage.subpage_types += ['polls.Question']
 
 
 class Question(Page):
-    parent_page_types = [
-        'core.LanguagePage', 'core.SectionPage', 'core.ArticlePage']
     subpage_types = ['polls.Choice']
 
     def choices(self):
