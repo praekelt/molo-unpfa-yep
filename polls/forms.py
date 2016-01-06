990e55c81a3d5eb7d34a3a78ca27ab78707380bf
codiebeulaine <codiebeulaine@gmail.com>
2016-01-06 09:45:10 +0200

    change styling

--- polls/forms.py
+++ polls/forms.py
@@ -7,12 +7,11 @@ class TextVoteForm(forms.Form):
 
 
 class VoteForm(forms.Form):
-    choice = forms.MultipleChoiceField(required=True)
+    choice = forms.MultipleChoiceField(
+        required=True,
+        error_messages={'required': "You didn't select a choice"})
 
     def __init__(self, *args, **kwargs):
         super(VoteForm, self).__init__(*args, **kwargs)
         self.fields['choice'].choices = [(
             c.pk, c.title) for c in Choice.objects.all()]
-
-    def clean_choice(self):
-        return self.cleaned_data['choice']
