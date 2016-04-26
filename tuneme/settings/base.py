2bbd1061114370ba5e0dbc339f65a39987765ec7
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-04-26 10:36:04 +0200

    added task to CELERY_IMPORTS

--- tuneme/settings/base.py
+++ tuneme/settings/base.py
@@ -212,7 +212,7 @@ GOOGLE_ANALYTICS_IGNORE_PATH = [
 
 # Celery
 
-CELERY_IMPORTS = ('google_analytics.tasks',)
+CELERY_IMPORTS = ('google_analytics.tasks', 'molo.profiles.task')
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
