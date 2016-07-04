a3151cd93bfd576525e267a3a22859353563b5bd
Peter Pistorius <peter.pistorius@gmail.com>
2016-07-04 17:12:44 +0200

    fixing languages highlight.

--- tuneme/settings/dev.py
+++ tuneme/settings/dev.py
@@ -5,6 +5,12 @@ DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+WAGTAILSEARCH_BACKENDS = {
+    'default': {
+        'BACKEND': 'wagtail.wagtailsearch.backends.db',
+    },
+}
+
 try:
     from .local import *  # noqa
 except ImportError:
