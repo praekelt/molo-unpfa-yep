f70706e33de05313b00cd6fcaff7af3912bb78d5
Milton Madanda <madandat@gmail.com>
2016-07-04 09:39:35 +0200

    3rd attempt - reference css directly

--- tuneme/settings/base.py
+++ tuneme/settings/base.py
@@ -284,7 +284,6 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_ROOT = join(PROJECT_ROOT, 'static')
-COMPRESS_ROOT = join(PROJECT_ROOT, 'tuneme', 'static', SITE_LAYOUT)
 STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = [
