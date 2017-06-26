1eb54c2346d3448b7b9b4cc6283f1924c9d18a34
Mitso <mqalaba@gmail.com>
2017-06-26 12:02:25 +0200

    update display survey directly setting

---
+++ tuneme/templatetags/surveypolls.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.assignment_tag
+def get_surveytype():
+    return "surveys"
+
+@register.assignment_tag
+def get_pollstype():
+    return "polls"
