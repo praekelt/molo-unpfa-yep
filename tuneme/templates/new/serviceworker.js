1884a650c1e069c3f8f9c8fd3edae227f35ef05c
Mitso <mqalaba@gmail.com>
2017-09-19 13:10:13 +0200

    update offline html file

--- tuneme/templates/new/serviceworker.js
+++ tuneme/templates/new/serviceworker.js
@@ -17,7 +17,7 @@ self.addEventListener('install', function(evt) {
 self.addEventListener('fetch', function(evt) {
   evt.respondWith(fetch(evt.request).catch(function () {
     return caches.open(CACHE).then(function(cache) {
-      return cache.match('/static/offline.html');
+      return cache.match('/offline.html');
     });
   }));
 });
@@ -26,7 +26,7 @@ function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
       '/',
-      '/static/offline.html',
+      '/offline.html',
       '/static/img/appicons/springster_icon_96.png',
       '/static/img/appicons/springster_icon_144.png',
       '/static/img/appicons/springster_icon_192.png'
