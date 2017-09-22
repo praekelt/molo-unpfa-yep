fd18ce51431ca21a2a09b598490911ebb8ff339d
Johan Bouwer <projectmushroom@me.com>
2017-09-22 10:57:16 +0200

    template cleanup

--- tuneme/templatetags/surveypolls.py
+++ tuneme/templatetags/surveypolls.py
@@ -4,21 +4,6 @@ from django.forms.fields import CheckboxInput
 register = template.Library()
 
 
-@register.assignment_tag
-def get_surveytype():
-    return "surveys"
-
-
-@register.assignment_tag
-def get_pollstype():
-    return "polls"
-
-
-@register.assignment_tag
-def get_competitiontype():
-    return "competition"
-
-
 @register.filter(name='is_checkbox')
 def is_checkbox(value):
     return isinstance(value, CheckboxInput)
