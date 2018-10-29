616ad1cd63ef6bea8986a5f69fc77544391e738d
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-10-29 14:20:27 +0200

    remove js script that is not used

--- tuneme/static/new/js/main.js
+++ tuneme/static/new/js/main.js
@@ -5,28 +5,6 @@
   };
 
   domReady(function() {
-    var selectRadius = document.getElementById('select_radius');
-    if(selectRadius){
-        console.log(selectRadius[selectRadius.selectedIndex].value);
-        // get selected option in sel (reference obtained above)
-        var optFunc = getSelectedOption(selectRadius);
-        selectRadius.addEventListener('click', function() {
-          console.log( optFunc.value," - ", optFunc.text);
-        });
-    }
-
-    function getSelectedOption(sel) {
-      var opt;
-      for ( var i = 0, len = sel.options.length; i < len; i++ ) {
-          opt = sel.options[i];
-            console.log("List options ",opt);
-          if ( opt.selected === true ) {
-            //First assign selected
-              break;
-          }
-      }
-      return opt;
-    }
 
     // Display select all field set
     // toggle selection on click
@@ -39,5 +17,6 @@
             $(this).find('input').prop('checked', $(selectAllKaywords).is(':checked'));
         });
     });
+
   });
 })();
