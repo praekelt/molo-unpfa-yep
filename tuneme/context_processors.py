57fd9f92c1aaf668ef9d0b890fc70c2696f95a30
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-10-29 11:20:11 +0200

    flake

--- tuneme/context_processors.py
+++ tuneme/context_processors.py
@@ -15,15 +15,15 @@ def default_forms(request):
 def add_tag_manager_account(request):
     site_settings = SiteSettings.for_site(request.site)
     return {
-        'GOOGLE_TAG_MANAGER_ACCOUNT': (site_settings.ga_tag_manager or
-                                       settings.GOOGLE_TAG_MANAGER_ACCOUNT)
+        'GOOGLE_TAG_MANAGER_ACCOUNT': (site_settings.ga_tag_manager
+                                       or settings.GOOGLE_TAG_MANAGER_ACCOUNT)
     }
 
 
 def detect_freebasics(request):
     return {
         'is_via_freebasics':
-            'Internet.org' in request.META.get('HTTP_VIA', '') or
-            'InternetOrgApp' in request.META.get('HTTP_USER_AGENT', '') or
-            'true' in request.META.get('HTTP_X_IORG_FBS', '')
+            'Internet.org' in request.META.get('HTTP_VIA', '')
+            or 'InternetOrgApp' in request.META.get('HTTP_USER_AGENT', '')
+            or 'true' in request.META.get('HTTP_X_IORG_FBS', '')
     }
